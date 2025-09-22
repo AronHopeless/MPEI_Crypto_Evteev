04dafd68d46148330574efa005e5ce45cde59cd1
Aron May <116968833+AronHopeless@users.noreply.github.com>
2025-09-22 12:17:13 +0300

    Update div2columns.py

--- KM1/div2columns.py
+++ KM1/div2columns.py
@@ -2,8 +2,9 @@ from collections import Counter
 
 text = ""
 
-for i in range(0, len(text) - 1, 4):
-    print(text[i]+" "+text[i+1]+" "+text[i+2]+" "+text[i+3])
+for i in range(0, len(text), 4):
+    chunk = text[i:i+4]          
+    print(" ".join(chunk))       
     
 print('\n\n____________________\n\n')
 
@@ -21,4 +22,5 @@ for idx, counter in enumerate(counters, start=1):
     print(f"\nСтолбец {idx}:")
     for symbol, count in counter.most_common():
         print(f"{symbol}: {count}")
+
 
